9937672886c80c3b5828149eef53da78545dabac
Héctor Fuentes <hfuentesg.infocred@bancochile.cl>
2020-01-09 15:41:55 -0300

    Fix user domain

--- src/app/core/domain/user.ts
+++ src/app/core/domain/user.ts
@@ -1,6 +1,21 @@
-export interface User {
-    uid: string
+export interface Roles {
+    admin: boolean
+    profile?: boolean
+}
+
+export class User {
     email: string
     displayName: string
-    emailVerified: string
+    photoURL?: string
+    roles: Roles
+
+    constructor(auth) {
+        this.email = auth.email
+        this.displayName = auth.displayName
+        this.photoURL = auth.displayName
+        this.roles = {
+            admin: false,
+            profile: true
+        }
+    }
 }

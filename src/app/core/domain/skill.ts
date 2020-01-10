7cea2528544cf0a431868b42b0891b6be488dd9f
alexis.veas <aveasg.imagemaker@bancochile.cl>
2020-01-10 15:23:14 -0400

    agregar skill - solo front

---
+++ src/app/core/domain/skill.ts
@@ -0,0 +1,6 @@
+export class Skill{
+  habilidad : string
+  experiencia: string
+  nivel: string
+  constructor(){}
+}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Skill } from '../core/domain/skill';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public skills:[Skill]
+  public agregar:boolean
+  habilidadSeleccionada :string;
+  experienciaSeleccionada: string
+  nivelSeleccionado:string
+
+  constructor() {
+this.skills = [{habilidad: "algo", experiencia:"3", nivel:"Junior" }];
+   }
 
   ngOnInit() {
+
+    this.skills.push({habilidad: "algo2", experiencia:"3", nivel:"Junior" });
   }
+
+  addItem():void {
+    this.agregar =false;
+    this.skills.push({habilidad:this.habilidadSeleccionada, experiencia:this.experienciaSeleccionada, nivel:this.nivelSeleccionado });
+}
+
+  cancelAddItem():void{
+    this.agregar = false;
+
+  }
+  selectChangeHandler (event: any) {
+      //update the ui
+      switch(event.target.name)
+      {
+        case "inputGroupSelectHabilidad":
+          this.habilidadSeleccionada = event.target.value;
+              break;
+       case "inputGroupSelectExperiencia":
+         this.experienciaSeleccionada = event.target.value;
+              break;
+      case "inputGroupSelectNivel":
+        this.nivelSeleccionado = event.target.value;
+            break;
+      }
+
+
+    }
 
 }

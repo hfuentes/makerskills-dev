3985985d6f05c63c24b1f1da724154b8ca1d87fd
jean.prouvay@gmail.com <jean.prouvay@gmail.com>
2020-01-16 10:52:44 -0300

    avance en Users de Admin

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -10,11 +10,23 @@ export class UsersComponent implements OnInit {
 
   constructor(public sharedService: SharedService) { }
 
+  private users: Array<any>;
+  private loading: boolean;
+  private error: any;
+
   ngOnInit() {
+    this.loading = true;
     this.sharedService.getUsers().then(users => {
-      console.log(users);
+      this.loading = false;
+      this.users = users;
+      console.log(this.users);
     }).catch(err => {
       console.error(err);
+      this.loading = false;
+      this.error = {
+        status: 400,
+        message: 'Servicio no disponible'
+      };
     });
   }
 

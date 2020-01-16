efe894ece35f25d6242662db543d94895d18be8e
Héctor Fuentes <hfuentesg.infocred@bancochile.cl>
2020-01-16 16:08:23 -0300

    Se eliminan imports no utilizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,6 @@ import { AuthGuard } from './core/auth.guard';
 import { ProfileComponent } from './profile/profile.component';
 import { RestrictedComponent } from './restricted/restricted.component';
 import { AdminComponent } from './admin/admin.component';
-import { UsersComponent } from './admin/users/users.component';
-import { SkillsComponent } from './admin/skills/skills.component';
-import { LevelsComponent } from './admin/levels/levels.component';
-import { ExpsComponent } from './admin/exps/exps.component';
-
 
 const routes: Routes = [
   { path: '', component: ProfileComponent, canActivate: [AuthGuard], data: { roles: ['profile'] } },

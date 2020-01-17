ba8a051d0204687a7ab62d0e92461993ad90ef72
jean.prouvay@gmail.com <jean.prouvay@gmail.com>
2020-01-17 16:32:51 -0300

    activacion de profile y admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth'
 import { AngularFirestoreModule } from '@angular/fire/firestore'
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { environment } from '../environments/environment'
 import { LoginComponent } from './login/login.component'
@@ -43,7 +44,8 @@ import { LevelsComponent } from './admin/levels/levels.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

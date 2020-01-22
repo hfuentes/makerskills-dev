b88ddaa8eb0b34bad29701a31210f36676c93811
jean.prouvay@gmail.com <jean.prouvay@gmail.com>
2020-01-22 12:01:15 -0300

    cambios en users

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../core/shared.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { User } from '../../core/domain/user';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/core/domain/user';
 
 @Component({
   selector: 'users',
@@ -11,11 +11,32 @@ import { User } from '../../core/domain/user';
 export class UsersComponent implements OnInit {
   public checkboxGroupForm: FormGroup;
 
+  forma: FormGroup;
+  addUser: boolean;
+
   constructor(
     public sharedService: SharedService,
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder) { 
+      this.forma = new FormGroup({
+        'email': new FormControl('', [
+          Validators.required,
+          Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+        ]
+        ),
+        'name': new FormControl('', 
+          Validators.required
+        ),
+        'activa': new FormControl(true),
+        'photoURL': new FormControl(''),
+        'roles': new FormGroup({
+          'admin': new FormControl(false),
+          'profile': new FormControl(true)
+        })
+      });
+    }
 
-  public users: Array<any>;
+  public usersFront: Array<UserFront>;
+  public users: Array<User>;
 
   public loading: boolean;
   public error: any;
@@ -35,16 +56,24 @@ export class UsersComponent implements OnInit {
   };
 
   ngOnInit() {
+    this.addUser = false;
+    this.usersFront = [];
     this.loadingGetUsers.loading = true;
     this.sharedService.getUsers().then(users => {
       this.loadingGetUsers.loading = false;
       this.users = users;
       users.forEach(user => {
-        user.checkboxGroupForm = this.formBuilder.group({
+
+        let userFront = new UserFront();
+
+        userFront.user = user;
+
+        userFront.checkboxGroupForm = this.formBuilder.group({
           profile: user.roles.profile,
           admin: user.roles.admin
         });
-        console.log(user.checkboxGroupForm.value.admin);
+
+        this.usersFront.push(userFront);
       });
 
     }).catch(err => {
@@ -57,20 +86,20 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  onClick(user){
-    if(user.active){
-      user.active = false;
+  onClick(userFront){
+    if(userFront.user.active){
+      userFront.user.active = false;
     }else{
-      user.active = true;
+      userFront.user.active = true;
     }
 
-    user.loadingUpdateUser = true;
-    this.sharedService.editUser(user.email, {active: user.active})
+    userFront.user.loadingUpdateUser = true;
+    this.sharedService.editUser(userFront.user.email, {active: userFront.user.active})
     .then(() => {
-      user.loadingUpdateUser = false;
+      userFront.user.loadingUpdateUser = false;
     })
     .catch(err => {
-      user.loadingUpdateUser = false;
+      userFront.user.loadingUpdateUser = false;
       this.loadingUpdateUser.error = {
         status: 400,
         message: 'Servicio no disponible'
@@ -78,18 +107,41 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  onClickRole(user, role){
-      if(user.roles[role] === true){
-        user.roles[role] = false;
-      }else{
-        user.roles[role] = true;
-      }
+  onClickRole(userFront, role){
+    let loading = role + 'RoleUserLoading';
+    userFront.user[loading] = true;
 
-    this.sharedService.editUser(user.email, {roles: user.roles})
+    if(userFront.user.roles[role] === true){
+      userFront.user.roles[role] = false;
+    }else{
+      userFront.user.roles[role] = true;
+    }
+
+    this.sharedService.editUser(userFront.user.email, {roles: userFront.user.roles})
     .then(() => {
-
+      userFront.user[loading] = false;
     })
     .catch(err => {
+      userFront.user[loading] = false;
     });
   }
+
+  guardarUsuario(){
+    console.log(this.forma.value);
+  }
+
+  addUserView(){
+    if(this.addUser){
+      this.addUser = false;
+    }else{
+      this.addUser = true;
+    }
+  }
+}
+
+export class UserFront {
+  user: User;
+  checkboxGroupForm: any;
+
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth'
 import { AngularFirestoreModule } from '@angular/fire/firestore'
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { environment } from '../environments/environment'
 import { LoginComponent } from './login/login.component'
@@ -47,7 +47,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AngularFireAuthModule,
     AngularFirestoreModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

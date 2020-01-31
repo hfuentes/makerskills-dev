7be0bc7d35cf774bb0da6e5aa9f7544aa826523c
Marcos Salinas <mssalinas.imagemaker@bancochile.cl>
2020-01-31 12:44:33 -0300

    Pantalla de creacion de tags

--- src/app/skills-tag/skills-tag.component.ts
+++ src/app/skills-tag/skills-tag.component.ts
@@ -10,20 +10,45 @@ export class SkillsTagComponent implements OnInit {
 
   public checkboxGroupForm: FormGroup;
   forma: FormGroup;
+  addUser: boolean;
 
-  constructor() { }
+  constructor(
+    
+    private formBuilder: FormBuilder) {
+      this.forma = new FormGroup({
+        'email': new FormControl(null, [
+          Validators.required,
+          Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+        ]
+        ),
+        'name': new FormControl(null,
+          Validators.required
+        ),
+        'activa': new FormControl(true),
+        'roles': new FormGroup({
+          'admin': new FormControl(false),
+          'profile': new FormControl(true)
+        })
+      });
+    }
 
   ngOnInit() {
+    this.addUser = false;
   }
 
   addUserView() {
-    console.log("Pase por aquí ...")
-    /*this.forma.reset({
+    console.log("aqui estoy !")
+    this.forma.reset({
       name: null,
       photoURL: null,
       roles: {admin: false, profile: true},
       active: true
-    });*/
+    });
+    if (this.addUser) {
+      this.addUser = false;
+    } else {
+      this.addUser = true;
+    }
   }
 
 }

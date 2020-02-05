49b93d6756f96836ff55bf49c0d98a900c326449
Jose Guerra <jguerra@imagemaker.com>
2020-02-05 11:03:38 -0300

    se cambia comportamiento de navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,7 +4,8 @@ import { NavbarMenuConstants } from '../constants/navbar-menu.constants'
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
